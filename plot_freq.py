d71d6f82ff5e2fafcb79ca6251976a5ffd1ca6dd
1-penny <pickwick2019@163.com>
2019-10-03 21:32:18 +0800

    create

---
+++ plot_freq.py
@@ -0,0 +1,56 @@
+# coding = utf-8
+
+'''
+绘制信号数据频谱图.
+'''
+
+import numpy as np
+import os
+
+
+def read_data(filename, dtype, dim=1, count=-1, offset=0):
+    ''' 读取数据.
+    @param dim 数据维度，1表示实数，2表示复数.
+    @param offset 跳过的数据量，以字节为单位.
+    '''
+    if not os.path.exists(filename) or os.path.getsize(filename) <= 0:
+        return None
+
+    data = np.fromfile(filename, dtype=dtype, count=count, offset=offset)
+
+    if dim == 2:
+        s = np.ndarray((int(len(data) / 2), ), dtype=np.complex)
+        s.real, s.imag = data[0::2], data[1::2]
+        return s
+    elif dim == 1:
+        s = np.ndarray((len(data), ), dtype=np.float)
+        s = data
+        return s
+    else:
+        return None
+
+
+def plot_freq(data):
+    ''' 绘制频谱.
+    '''
+    import scipy.fftpack
+    import matplotlib.pyplot as plt
+
+    if np.iscomplexobj(data):
+        data_f = scipy.fft(data)
+    else:
+        data_f = scipy.fftpack.rfft(data)
+
+    data_f = np.abs(data_f)
+    data_f = np.log10(data_f) * 10
+
+    plt.plot(data_f)
+    plt.show()
+
+
+if __name__ == '__main__':
+    filename = "c:/data/gpssim.bin"
+    data = read_data(filename, np.int16, count=1024*2)
+
+    if data is not None:
+        plot_freq(data)
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@ DSP Python 处理代码集合.
 ## compare_file.py
 
 比较两个文件（按二进制内容对比），并输出比较结果信息.
+
+## plot_freq.py
+
+绘制信号数据频谱图.
